1867742e3a023efc5a677176837249153fcbced0
AAA <gomel.city@mail.ru>
2020-03-21 12:57:56 +0300

    calc

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+function getValues (){
+    var result = [];
+    var i = 0;
+do {
+   var result[i] = +prompt ('Введите число 1'); 
+   i++
+   var isProcess = confirm ('Продолжить?')
+} while (isProcess);
+for (var m=0){}
+alert ('Результат: ' + result);
+}
